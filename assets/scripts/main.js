47d0238378c451e0c0055520e3bca796d054baba
Paul Goldman <paulgoldmanmusic@gmail.com>
2017-08-02 06:58:18 -0500

    Added animation.css, added contact form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,28 +1,37 @@
 // When the Document Object Model is ready
 $(document).ready(function(){
 
+	$(".nav a").on("click", function(){
+	   $(".nav").find(".active").removeClass("active");
+	   $(this).parent().addClass("active");
+	});
 
-	// $(document).on('scroll', function (e) {
-	// 	// var alpha = $(document).scrollTop() / 550;
-	//
-    // //  $('.navbar-default').css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
-	// });
+  // (document).on('scroll', function (e) {
+  // // var alpha = $(document).scrollTop() / 550;
+  //
+  //   //  $('.navbar-default').css('background-color', 'rgba(255, 255, 255,' + alpha + ')');
+  //
+  //
+  // /   'aboutTopPosition' is the amount of pixels #about
+  // /   is from the top of the document
+  // /   var aboutTopPosition = $('#about-row').offset().top;
+  // /   $('#about-row').click(function(){
+  // /   // Scroll down to 'aboutTopPosition'
+  // /   $('html, body').animate({scrollTop:aboutTopPosition}, 'slow');
+  // /   });
+  //  //
+  // /     	var portfolioTopPosition = $('#portfolio-row').offset().top;
+  // /   $('#portfolio-row').click(function(){
+  // /   $('html, body').animate({scrollTop:portfolioTopPosition}, 'slow');
+  // /   });
+  //  //
+  // /      	var contactTopPosition = $('#contact-row').offset().top;
+  // /   $('#contact-row').click(function(){
+  // /   $('html, body').animate({scrollTop:contactTopPosition}, 'slow');
+  // /   });
+  // });
 
-  / 'aboutTopPosition' is the amount of pixels #about
-  / is from the top of the document
-  ar aboutTopPosition = $('#about').offset().top;
-  ('#about-link').click(function(){
-  // Scroll down to 'aboutTopPosition'
-  $('html, body').animate({scrollTop:aboutTopPosition}, 'slow');
-  });
 
-    	var portfolioTopPosition = $('#portfolio').offset().top;
-  ('#portfolio-link').click(function(){
-  $('html, body').animate({scrollTop:portfolioTopPosition}, 'slow');
-  });
 
-     	var contactTopPosition = $('#contact').offset().top;
-  ('#contact-link').click(function(){
-  $('html, body').animate({scrollTop:contactTopPosition}, 'slow');
-  });
+
 });
